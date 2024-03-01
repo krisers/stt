b6434d90ec927bd945c33101964692afbecde23a
krisers <kriserstream@gmail.com>
2024-03-01 04:21:07 +0300

    7b

--- summerize_batch.py
+++ summerize_batch.py
@@ -1,7 +1,13 @@
 import os 
 
 if __name__ == "__main__":
-    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-14B-Chat' --text_filename='b99s04e03.srt' " )
-    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-14B-Chat' --text_filename='b99s04e04.srt' " )
-    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-14B-Chat' --text_filename='b99s04e05.srt' " )
-    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-14B-Chat' --text_filename='b99s04e06.srt' " )
+    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-7B-Chat' --text_filename='b99s04e03.srt' " )
+    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-7B-Chat' --text_filename='b99s04e04.srt' " )
+    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-7B-Chat' --text_filename='b99s04e05.srt' " )
+    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-7B-Chat' --text_filename='b99s04e06.srt' " )
+    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-7B-Chat' --text_filename='nanny.srt' " )
+    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-7B-Chat' --text_filename='summer.srt' " )
+    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-7B-Chat' --text_filename='lawandorder.srt' " )
+    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-7B-Chat' --text_filename='arrival.srt' " )
+    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-7B-Chat' --text_filename='theniceguys.srt' " )
+    os.system("python test_qwen.py --device='cpu' --model_name='Qwen/Qwen1.5-7B-Chat' --text_filename='theotherguys.srt' " )
